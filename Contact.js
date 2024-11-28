004491e215637115f313e6978fa74f70593b9d95
Rehab Mahmoud <138263003+Rehab-M-Esmail@users.noreply.github.com>
2024-11-28 09:23:27 +0200

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -1,12 +1,19 @@
 const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 function validateInput()
 {
-    email = document.getElementById("Email")
+    console.log("input validation")
+    email = document.getElementById("Email").value
+    console.log(email);
     if(pattern.test(email))
     {
     //const modal = document.createElement("Modal");
-    document.getElementById("modal").style.visibility= "visible";
-    document.getElementById("container").style.visibility="hidden";
+    //document.getElementById("modal").style.visibility= "visible";
+    //document.getElementById("container").style.visibility="hidden";
+    window.alert("Thanks for contacting us");
+    }
+    else
+    {
+        window.alert("Please enter a valid email address.");
     }
 }
 document.getElementById("submitBtn").addEventListener("click",validateInput)
